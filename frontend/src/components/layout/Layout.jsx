a0b4c9bbd1a69eb37f79752423e1afa32a74c64b
bajoelabrigo <bajoelabrigo@gmail.com>
2024-09-25 16:46:03 -0500

    first commit

---
+++ frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+
+  
+  
+  return (
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main className="max-w-7xl mx-auto px-4">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
